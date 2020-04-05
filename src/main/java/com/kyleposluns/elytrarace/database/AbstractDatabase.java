7f5de969d38c82b9f170cd0a1625046faf431824
Kyle Posluns <kyleposluns@gmail.com>
2020-04-05 14:53:09 -0400

    Removed unused adapters.

--- src/main/java/com/kyleposluns/elytrarace/database/AbstractDatabase.java
+++ src/main/java/com/kyleposluns/elytrarace/database/AbstractDatabase.java
@@ -1,26 +1,14 @@
 package com.kyleposluns.elytrarace.database;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.kyleposluns.elytrarace.arena.area.AreaTypeAdapterFactory;
-import com.kyleposluns.elytrarace.arena.LocationAdapter;
-import com.kyleposluns.elytrarace.records.Record;
-import com.kyleposluns.elytrarace.records.RecordAdapter;
 import com.kyleposluns.elytrarace.records.RecordBook;
 import java.util.UUID;
-import org.bukkit.Location;
 
 public abstract class AbstractDatabase implements ElytraDatabase {
-
-  protected final Gson gson;
 
   protected final Credentials credentials;
 
   public AbstractDatabase(Credentials credentials) {
     this.credentials = credentials;
-    this.gson = new GsonBuilder().registerTypeAdapter(Record.class, new RecordAdapter())
-        .registerTypeAdapter(Location.class, new LocationAdapter())
-        .registerTypeAdapterFactory(new AreaTypeAdapterFactory()).create();
   }
 
   protected abstract RecordBook findPlayerRecordBook(UUID playerId);
